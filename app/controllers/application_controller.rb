e9104e59c2e16209f1e008439873dfe6e0b3de0d
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-05-17 16:02:20 -0400

    breakfast time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,42 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+
   get '/' do
+    redirect '/recipes'
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id]).update(params[:edit])
+    redirect "/recipes/#{params[:id]}"
+  end
+
+  delete '/recipes/:id/delete' do
+    Recipe.find(params[:id]).delete
+    redirect "/recipes"
   end
 end
